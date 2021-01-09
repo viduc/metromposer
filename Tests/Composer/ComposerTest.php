d08ba0b040675adc76b9c1575b8b34664fc11d4b
Tristan F <tristan.fleury@univ-grenoble-alpes.fr>
2021-01-09 16:30:33 +0100

    modification fct test unitaire

--- Tests/Composer/ComposerTest.php
+++ Tests/Composer/ComposerTest.php
@@ -29,7 +29,9 @@ class ComposerTest extends TestCase
     final public function testGenererLaListeDesLibrairiesAmettreAjour() : void
     {
         $config = new Configuration();
-        mkdir($config->recupererPathApplication() . '/metromposer');
+        if (!is_dir($config->recupererPathApplication() . '/metromposer')) {
+            mkdir($config->recupererPathApplication() . '/metromposer');
+        }
         $configuration = $this->createMock(ConfigurationInterface::class);
         $configuration->method('recupereUnParametre')->willReturn('test');
         $this->composer->setConfiguration($configuration);
